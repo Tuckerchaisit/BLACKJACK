6253caa94fa2fcdca4f9222eebfc734cfc4e458f
Tucker <tucker.chaisit@gmail.com>
2022-02-11 10:55:12 -0500

    finish pickcard function

--- js/app.js
+++ js/app.js
@@ -23,8 +23,9 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+let deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
+let winner;
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -33,12 +34,45 @@ const dealerCards = Array.from(document.querySelectorAll(".dealer"));
 const playerCards = Array.from(document.querySelectorAll(".player"));
 const hitBtn = document.querySelector(".hit");
 const standBtn = document.querySelector(".stand");
-const playAgainBtn = document.querySelector(".new-game");
+const StartNewGame = document.querySelector(".new-game");
 
 /*----------------------------- Event Listeners -----------------------------*/
 hitBtn.addEventListener('click',handleHit);
 standBtn.addEventListener('click',handleStand);
-playAgainBtn.addEventListener('click',init);
+StartNewGame.addEventListener('click',handleStart);
 
 
 /*-------------------------------- Functions --------------------------------*/
+init();
+
+function init(){
+
+  deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+  dealHand = [null, null, null, null, null];
+  playHand = [null, null, null, null, null];
+  msgStat.innerHTML = "Press Start-New-Game to start the game!";
+  pickCard();
+  render();
+}
+
+function handleStart(){
+
+}
+
+function render(){
+
+}
+
+function handleHit(){
+
+}
+
+function handleStand(){
+
+}
+
+function pickCard(){
+  let randIdx = Math.floor(Math.random()*deck1.length);
+  let cardPicked = deck1.splice(randIdx, 1)[0];
+  console.log(cardPicked);
+}
