376cd1e726321ed0b5c21624e8748130440e041f
Tucker <tucker.chaisit@gmail.com>
2022-02-13 23:50:07 -0500

    solve issue with grid layout not working during larger display

--- js/app.js
+++ js/app.js
@@ -11,11 +11,12 @@
 // // [TODO:]Define required constant and winning condition
 // // [TODO:]Handle player clicking hit button or stand button to start the game
 // // [TODO:]Handle a player clicking the Play again button.
+// // [TODO:]Add a favicon to our site
 
 
 // [TODO:]Add responsive design
 // [TODO:]Add google Fonts
-// [TODO:]Add a favicon to our site
+
 
 
 
@@ -27,7 +28,6 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
-let winner;
 let cardPicked;
 let playerPoints;
 let dealerPoints;
@@ -93,7 +93,7 @@ function isBJ(){
       msgStat.innerHTML= "The player has won"
       stopHitStand();
     }
-  }else{
+      }else{
     if(playerPoints>21){
       msgStat.innerHTML= `The player has lost, Player score exceed 21!`
       stopHitStand();
